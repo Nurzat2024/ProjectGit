2036f37e91d8d984ef41b199c71362406827afc6
Nurzat <nurzat.zarylbekova.ch@gmail.com>
2024-01-28 17:38:11 +0600

    Feature/Iphone15-1

--- src/apple1/IPhone15.java
+++ src/apple1/IPhone15.java
@@ -3,5 +3,6 @@ package apple1;
 public class IPhone15 {
     public static void main(String[] args) {
         System.out.println("my first iphone 15");
+        System.out.println("my second iphone 15");
     }
 }
