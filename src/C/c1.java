abef6b71f745cac5344e1612d6c5934f80f95090
Nurzat <nurzat.zarylbekova.ch@gmail.com>
2024-01-28 16:35:09 +0600

    Revert "C1-commit-updated"
    This reverts commit df942a7d45e8bbe2215ee0c77f34cf17ce9f28fe.

--- src/C/c1.java
+++ src/C/c1.java
@@ -4,10 +4,4 @@ public class c1 {
     public static void main(String[] args) {
         System.out.println("Hello world");
     }
-
-    public static void seyHello() {
-        System.out.println("Hello");
-
-    }
 }
-
