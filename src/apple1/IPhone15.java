e9e6be0551db2533f23ef97d2421bfbb1c8d8001
Nurzat <nurzat.zarylbekova.ch@gmail.com>
2024-01-28 17:39:47 +0600

    Revert "Feature/Iphone15-2"
    This reverts commit 2036f37e91d8d984ef41b199c71362406827afc6.

--- src/apple1/IPhone15.java
+++ src/apple1/IPhone15.java
@@ -3,6 +3,5 @@ package apple1;
 public class IPhone15 {
     public static void main(String[] args) {
         System.out.println("my first iphone 15");
-        System.out.println("my second iphone 15");
     }
 }
